7335a247f6abee7b6b9a623b48067db96193322e
NaterForst <olivier.petit007@orange.fr>
2017-06-22 20:12:22 +0200

    ignore yarn.lock/ignore intellij/Changement port ecoute Front/ ETape 2/ ETape3

---
+++ frontend/src/components/timeline.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="timeline">
+    <h1>{{msg}}</h1>
+    <ul>
+           <li v-for="tweet in tweets">
+
+                {{ tweet }}
+           </li>
+    </ul>
+
+
+   </div>
+</template>
+
+<script>
+export default {
+  name: 'timeline',
+  data () {
+    return {
+      tweets: ['tweet 1', 'tweet 2', 'tweet 3']
+    }
+  }
+
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
