774cded2d26c3c04077fc513cc867279fca78275
NaterForst <olivier.petit007@orange.fr>
2017-06-23 11:58:16 +0200

    etape 8 et 9

--- frontend/src/components/timeline.vue
+++ frontend/src/components/timeline.vue
@@ -1,60 +1,53 @@
 <template>
   <div class="timeline">
-    <ul>
-      <li v-for="tweet in tweets">
-        <tweet :tweet="tweet"/>
-      </li>
-    </ul>
+    <h1>{{ msg }}</h1>
+    <div v-if="loading">Chargement des tweets en cours…</div>
+    <div v-else>
+      <feed :tweets="tweets"/>
+    </div>
   </div>
 </template>
 
 <script>
 
-  import Tweet from './Tweet'
-  
+// Imports
+import Feed from './Feed'
+import Vue from 'vue'
+import Resource from 'vue-resource'
+Vue.use(Resource)
 
-  export default {
-    name: 'timeline',
-    data () {
-      return {
-        tweets: [{auteur: 'proutt', contenu: 'yihiii'}, {auteur: 'dsdksds', contenu: 'djsdjsds'}, {auteur: 'dsjdhsjdhs', contenu: 'dysgdysgd'}]
-      }
-    },
-    components: {Tweet},
-    methods: {
-      fetchTweets: function () {
-        this.$http.get('http://localhost:8080/list').then(response => {
-          // get body data
-          this.tweets = response.body
-          console.log(response.body)
-        }, response => {
-          // error callback
-        })
-      }
-    },
-    created () {
-      this.fetchTweets()
+export default {
+  name: 'timeline',
+  components: {Feed},
+
+  data () {
+    return {
+      msg: 'Timeline Twitter',
+      tweets: [],
+      loading: true
+    }
+  },
+
+  created () {
+    this.fetchTweets()
+  },
+
+  methods: {
+    fetchTweets: function () {
+      // GET /list
+      this.$http.get('http://localhost:8080/list').then(response => {
+        // get body data
+        this.tweets = response.body
+        this.loading = false
+        console.log(response)
+      }, response => {
+        this.loading = true
+      })
     }
   }
+}
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-  h1, h2 {
-    font-weight: normal;
-  }
+<style scoed>
 
-  ul {
-    list-style-type: none;
-    padding: 0;
-  }
-
-  li {
-    display: block;
-    margin: 0 10px;
-  }
-
-  a {
-    color: #42b983;
-  }
 </style>
