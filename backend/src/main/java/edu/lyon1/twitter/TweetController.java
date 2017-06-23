c8079f895bfb14600f01ed8c5d77a4b822e49405
NaterForst <olivier.petit007@orange.fr>
2017-06-23 09:25:28 +0200

    @CrossOrigin

--- backend/src/main/java/edu/lyon1/twitter/TweetController.java
+++ backend/src/main/java/edu/lyon1/twitter/TweetController.java
@@ -8,9 +8,11 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import java.util.List;
 import java.util.Optional;
+@CrossOrigin(origins = "http://localhost:8081")
 
 @RestController
 public class TweetController {
